82966fb91f1d0fa63a8ae4596dff30269da7156f
jeroqui <ovnijacob@gmail.com>
2025-07-25 12:08:06 +0200

    article 16.2

--- carretera.py
+++ carretera.py
@@ -8,15 +8,20 @@ class Calçada:
     ...
 
 class Carril(Calçada):
-    ...
-
+    es_lent = False
+    es_reservat = False
 
 class Carretera:
 
     carrils = []
 
+    def __len__(self) -> int:
+        # Article 16.2
+        return len(list(filter(lambda x: not (x.es_reservat or x.es_lent), self.carrils)))
+
+
     def __init__(self, carrils: list[Carril]):
-        if len(carrils) == 0:
+        if len(self) == 0:
             raise Exception("La carretera ha de tenir almenys un carril")
 
         self.carrils = carrils
